2ff9bf25796fe8c06e4b9f9b49f4ffc673c9b86b
David Castillo <dcastil1983@gmail.com>
2014-04-30 13:35:21 -0700

    Rnadom things
    Added auto targetting and rotation

--- Assets/Scripts/AllyUnit.cs
+++ Assets/Scripts/AllyUnit.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class AllyUnit : baseUnit {
 	
 	public Vector3 targetLocation;
+	public float lastAttack = 0f;
+	public Rigidbody projectile;
+	public Vector3 offset;
+	public float velocity = 100;
+	public int delay = 3;
+
+	public AudioSource unitSound;
+	public AudioClip fireSound;
 	
     public bool canMove;
     public int energy;
@@ -12,9 +20,30 @@ public class AllyUnit : baseUnit {
 	void Start () {
 		base.Start();
 	}
+
+	  EnemyUnit FindClosestEnemy() 
+	  {
+        EnemyUnit[] gos;
+        gos = EnemyUnit.gameObject.FindGameObjectsWithTag("Enemy");
+        EnemyUnit closest = FindObjectOfType<EnemyUnit>();
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (EnemyUnit go in gos) 
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance) 
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
         if (currentHealth <= 0)
         {
             Destroy(gameObject.rigidbody);
@@ -28,6 +57,27 @@ public class AllyUnit : baseUnit {
 		else{
 			renderer.material = defaultMaterial;
 		}
+
+
+	if(!currentTarget)
+	{
+		currentTarget = FindObjectOfType<EnemyUnit>();	
+	}
+	
+	transform.LookAt(currentTarget.transform);
+    if (Vector3.Distance(transform.position, currentTarget.transform.position) <= attackRange && lastAttack >= attackRate)
+    {
+    	
+        Rigidbody clone;
+        clone = (Rigidbody)Instantiate(projectile, transform.position + offset, transform.rotation);
+		unitSound.PlayOneShot(fireSound, 1);
+        clone.velocity = transform.TransformDirection(Vector3.forward * velocity) + new Vector3(Time.deltaTime * velocity, 0, 0);
+        //removeObject(clone, 3);
+        lastAttack = 0;
+        Destroy(clone, delay);
+        Destroy(clone.gameObject, delay);
+    }
+    lastAttack += Time.deltaTime;
 
 	}
 	/*void OnMouseEnter(){
@@ -43,4 +93,7 @@ public class AllyUnit : baseUnit {
 			updateRightClick = false;
 		}
 	}
+
+
+
 }
--- Assets/Scripts/EnemyFire.cs
+++ Assets/Scripts/EnemyFire.cs
@@ -4,17 +4,39 @@ using System.Collections;
 public class EnemyFire : MonoBehaviour
 {
 
+/*
     public float fireRate;
     public int damage;
     public int range;
     public AllyUnit target;
+*/
+
+    public GameObject EnemyProjectile;
+    public GameObject allyUnit;
+    public GameObject enemyUnit;
+    public float reloadTime = 1f;
+    public float turnSpeed = 5f;
+    public float firePauseTime = .25f;
+    public Transform myTarget;
+    public Transform turret;//turretball
+    public Transform[] muzzlePositions;
+    public float range = 10000;
+
+    private double nextFireTime;
+    private float nextMoveTime;
+    private Quaternion desiredRotation;
 
 
     void Update()
     {
 
+
     }
-    /*
+
+
+/*
+    }
+    
     void OnCollisionEnter(Collision collision) {
         ContactPoint contact = collision.contacts[0];
         //Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
@@ -24,3 +46,4 @@ public class EnemyFire : MonoBehaviour
     }
     */
 }
+
