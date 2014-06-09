bfc2085450050c2b774e11b47ecef229fd482def
David Castillo <dcastil1983@gmail.com>
2014-06-08 18:46:52 -0700

    Fixed conflict
    Removed a few lines of code to fix a merge conflict

--- Assets/Scripts/AllyUnit.cs
+++ Assets/Scripts/AllyUnit.cs
@@ -30,16 +30,10 @@ public class AllyUnit : baseUnit
 
 				playSound = true;
 				if (mainslot) {
-<<<<<<< HEAD
-						health = mainslot.healthBoost;
-                        health += secondary.healthBoost;
-                        currentHealth = health;
-                }				
-=======
 						health = mainslot.healthBoost + secondary.healthBoost;
 						currentHealth = health;
 				}				
->>>>>>> 4135980dd17e6d86867333a28eacf50d8408d4dd
+
 				if (mainslot) {
 						attackDmg = mainslot.damage;
 				}
